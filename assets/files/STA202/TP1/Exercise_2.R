5be8288fe7dee5e3df870380eb7637f6cbaf09fc
mzaffran <margauxzaffran@gmail.com>
2022-02-02 15:43:47 +0100

    updated exercise 2

--- assets/files/STA202/TP1/Exercise_2.R
+++ assets/files/STA202/TP1/Exercise_2.R
@@ -6,10 +6,9 @@ setwd("~/Documents/TDs/STA202/TP1/TP1_data")
 
 ###### Data import
 
-data = read.table("conso_2015.csv", header=T, sep=';')
+data = read.csv("conso_2015.csv", header=T, sep=';')
 
 ###### First visualization
-
 
 summary(data)
 head(data) 
@@ -21,11 +20,13 @@ head(data)
 
 ## Date creation
 
+# head displays the first 6 elements (by defaut, set n=k to obtain the k first elements)
 head(data$Date) # Begins the 01/01/2015
+# tail is the same than head, for the last elements
 tail(data$Date) # Ends the 30/11/2015
 
-date1 = strptime("01/01/2015 00:00:00", "%m/%d/%Y %H:%M:%S")
-date2 = strptime("11/30/2015 23:30:00", "%m/%d/%Y %H:%M:%S")
+date1 = strptime("01/01/2015 00:30:00", "%m/%d/%Y %H:%M:%S")
+date2 = strptime("11/30/2015 24:00:00", "%m/%d/%Y %H:%M:%S")
 Date = seq(date1, date2, by = "30 min")
 summary(Date)
 head(Date)
@@ -33,17 +34,22 @@ head(Date)
 ## Time serie
 
 X = as.matrix(t(data[,-1])) 
-# All column except the first one (the date), and transposed: each column corresponds to one day
+# All column except the first one (the date), 
+# and transposed: each column now corresponds to one day
 conso = c(X)
-# conso is the concatenation (by columns) of the matrix X: each column has been put after the previous one.
+# we transform X to a vector
+# conso is the concatenation (by columns) of the matrix X: 
+# each column has been put after the previous one.
 
-plot(Date[1:(48*7*3)],conso[1:(48*7*3)], type='l', ylab="Electricity consumption") # Plots only the 3 first weeks of consumption
+plot(Date[1:(48*7*3)],conso[1:(48*7*3)], type='l', ylab="Electricity consumption", xlab='Date') # Plots only the 3 first weeks of consumption
 plot(Date, conso, type='l', ylab="Electricity consumption") # Plots all the data
 
 library(xts)
 
 conso.xts = xts(conso, order.by=Date)
+# note again here that we use the Date vector previously created
 plot(conso.xts)
+# xts makes automatically beautiful plots!
 
 ###### Descriptive analysis
 
@@ -57,6 +63,8 @@ mean(conso.xts)
 
 month = as.factor(.indexmon(conso.xts))
 mean.month = tapply(conso.xts, month, mean)
+# we could also use the code of Exercise 1, 
+# notice it is the same with another way of creating the factor.
 plot(mean.month, type='b', xlab="Month", ylab="Electricity consumption", 
      main="Averaged electricity consumption per month")
 
@@ -82,6 +90,11 @@ abline(v=seq(1, 24*7, by=24), col='red')
 col.pal = colorRampPalette(c("lightblue", "red"))( 12 )
 sel = which(.indexhour(conso.xts)==20) # indexes of data at 8PM
 boxplot(conso[sel] ~ month[sel], col=col.pal, xlab="Months", ylab="Electricity consumption")
+# In the function boxplot, you can specify a formula "A ~ B" to show 
+# the boxplots of A conditionnally on the factor B.
+# Here, we also specify [sel] to select only the rows of indexes in sel,
+# where sel was created before to contain only row indexes of hour 20.
+# We only had 11 months: only one data in December in the data set.
 
 ###### Autocorrelations
 
