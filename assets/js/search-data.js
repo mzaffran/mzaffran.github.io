1d18f9273c6eefe394be892ecdee3e4dc7b1241a
mzaffran <40029617+mzaffran@users.noreply.github.com>
2025-07-18 04:32:38 +0000

    Deploying to gh-pages from @ mzaffran/mzaffran.github.io@6b6e64e78270f3266d2979bc4114bc9da256aed2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -497,7 +497,7 @@ ninja.data = [{
         title: 'Bluesky',
         section: 'Socials',
         handler: () => {
-          window.open("mzaffran", "_blank");
+          window.open("https://bsky.app/profile/mzaffran.bsky.social", "_blank");
         },
       },{
         id: 'social-linkedin',
